173dbf8ae7fa1233fe2e4aa620a011e0b78f4e6e
Daria Sevostianova <d.sevostianova@innopolis.university>
2026-09-02 15:11:52 +0300

    New maven project and start of cli

---
+++ demo/src/main/java/com/example/demo/TaskTracker.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+
+enum Priority {
+	HIGH, MEDIUM, LOW
+}
+
+@Command(name = "task-cli", mixinStandartHelpOptions = true, version = "1.0", description = "bebbe")
+public class TaskTracker implements Runnable{
+	@Parameters(index = "0")
+	private String command;
+
+	//Order matters
+	@Parameters(index = "1..*")
+	private String[] commandArgs;
+
+	//@Option - no matter when we put needed command
+	@Option(names = {"-p", "--priority"}, description = "assigns priorities")
+	private Priority priority = Priority.MEDIUM;
+	
+	public static void main(String[] args) {
+
+	}
+
+}
